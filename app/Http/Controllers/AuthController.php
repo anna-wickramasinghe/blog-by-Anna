60c9b5dcfb05d6fb36eb6da34462f188c2526e7b
anna-wickramasinghe <nprawickramasinghe@gmail.com>
2024-09-24 21:33:14 +0530

    add user registration functionality

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,8 +3,37 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Validation\ValidationException;
 
 class AuthController extends Controller
 {
-    //
+    public function register(Request $request)
+    {
+        $validated = $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:8',
+        ]);
+
+        $user = User::create([
+            'name' => $validated['name'],
+            'email' => $validated['email'],
+            'password' => Hash::make($validated['password']),
+        ]);
+
+        return response()->json(['message' => 'User Created Successfully!'], 201);
+    }
+
+    public function login(Request $request)
+    {
+        // login
+    }
+
+    public function logout(Request $request)
+    {
+        // logout
+    }
 }
