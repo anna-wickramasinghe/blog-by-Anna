58dce64a91671f975f254712c9f7919d8cba10c4
anna-wickramasinghe <nprawickramasinghe@gmail.com>
2024-09-24 21:48:45 +0530

    add user logout functionality

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -49,6 +49,7 @@ class AuthController extends Controller
 
     public function logout(Request $request)
     {
-        // logout
+        $request->user()->tokens()->delete();
+        return response()->json(['message' => 'Successfully logged out']);
     }
 }
